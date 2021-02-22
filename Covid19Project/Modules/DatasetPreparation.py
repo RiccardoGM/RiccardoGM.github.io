5641ed3a9e039bc39fb770a8273dd896c9220f89
RiccardoGM <63930826+RiccardoGM@users.noreply.github.com>
2021-02-22 17:40:17 +0000

    Update DatasetPreparation.py

--- Covid19Project/Modules/DatasetPreparation.py
+++ Covid19Project/Modules/DatasetPreparation.py
@@ -12,30 +12,30 @@ import numpy as np
 
 
 def trainingset_preparation(DataFrame=pd.DataFrame()):
-
+    
     '''
        This function prepares the training set for pre-processing.
-
-       Input:
+       
+       Input: 
              DataFrame = .xlsx file with all covariates as in the reference dataset.
              If no dataframe is passed to the function, the reference dataset
-             will be loaded from 'https://RiccardoGM.github.io/Covid19Project/Data/FlowCitometryData.xlsx'.
+             will be loaded from '..URL to be added..'.
     '''
-
-
+    
+    
     # Load ref. dataset
-    StrPath = 'https://RiccardoGM.github.io/Covid19Project/Data/FlowCitometryData.xlsx' #URL or local path
+    StrPath = '/Users/Riccardo/TriesteProject/Covid19/Datasets/SecondWave_Citofluorimetria/FlowCitometryData.xlsx' #URL or local path
     DataRef = pd.read_excel(StrPath, engine='openpyxl')
     ColumnsRef = DataRef.columns.values.copy()
-
-
+    
+    
     # Assign dataset to be used as training set
     if not DataFrame.empty:
-
+        
         # create local copy
         Data = DataFrame.copy()
         Columns = Data.columns.values.copy()
-
+        
         # check consistency with ref. dataset
         if np.array_equal(Columns, ColumnsRef):
             pass
@@ -43,14 +43,23 @@ def trainingset_preparation(DataFrame=pd.DataFrame()):
             StrError1 = '; '.join(Columns)+';'
             StrError2 = '; '.join(ColumnsRef)+';'
             raise ValueError('Wrong column format.\nInserted format:\n%s\nExpected format:\n%s' %(StrError1, StrError2))
-
+    
     else:
-
+        
         # use ref. dataframe
         Data = DataRef.copy()
         Columns = ColumnsRef.copy()
-
-
+    
+    
+    # Print info about covariates
+    N_samples = Data.shape[0]
+    N_covariates = Data.shape[1]
+    for i, element in enumerate(Columns):
+        if 'date' not in element:
+            N_data = pd.count(Data[element], numeric_only=True)
+            print(element, 'N available data: %d/%d' % (N_data, N_samples))
+        
+    
     # Return prepared dataset
     return Data
 
