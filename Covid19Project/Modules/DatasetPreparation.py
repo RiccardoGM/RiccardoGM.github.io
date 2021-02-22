43e9dc3debe9842c45be0aa8b58bb2f46d5c7a31
RiccardoGM <63930826+RiccardoGM@users.noreply.github.com>
2021-02-22 16:58:41 +0000

    Update DatasetPreparation.py

--- Covid19Project/Modules/DatasetPreparation.py
+++ Covid19Project/Modules/DatasetPreparation.py
@@ -12,30 +12,30 @@ import numpy as np
 
 
 def trainingset_preparation(DataFrame=pd.DataFrame()):
-    
+
     '''
        This function prepares the training set for pre-processing.
-       
-       Input: 
+
+       Input:
              DataFrame = .xlsx file with all covariates as in the reference dataset.
              If no dataframe is passed to the function, the reference dataset
              will be loaded from '..URL to be added..'.
     '''
-    
-    
+
+
     # Load ref. dataset
-    StrPath = '/Users/Riccardo/TriesteProject/Covid19/Datasets/SecondWave_Citofluorimetria/FlowCitometryData.xlsx' #URL or local path
+    StrPath = 'https://RiccardoGM.github.io/Covid19Project/Data/FlowCitometryData.xlsx' #URL or local path
     DataRef = pd.read_excel(StrPath, engine='openpyxl')
     ColumnsRef = DataRef.columns.values.copy()
-    
-    
+
+
     # Assign dataset to be used as training set
     if not DataFrame.empty:
-        
+
         # create local copy
         Data = DataFrame.copy()
         Columns = Data.columns.values.copy()
-        
+
         # check consistency with ref. dataset
         if np.array_equal(Columns, ColumnsRef):
             pass
@@ -43,14 +43,14 @@ def trainingset_preparation(DataFrame=pd.DataFrame()):
             StrError1 = '; '.join(Columns)+';'
             StrError2 = '; '.join(ColumnsRef)+';'
             raise ValueError('Wrong column format.\nInserted format:\n%s\nExpected format:\n%s' %(StrError1, StrError2))
-    
+
     else:
-        
+
         # use ref. dataframe
         Data = DataRef.copy()
         Columns = ColumnsRef.copy()
-        
-    
+
+
     # Return prepared dataset
     return Data
 
